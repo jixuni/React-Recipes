db650893677e3c408ee50f4a079722f06805dc4b
Jixu Ni <jixuni@Jixus-MBP.home>
2019-05-20 10:01:10 -0400

    Updates

---
+++ precache-manifest.14a4fb4ebd19ca35ff15988526bf0337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3f6e1c7ece64ff0357c5ac49af392c",
+    "url": "/React-Recipes/index.html"
+  },
+  {
+    "revision": "c7993d6f2f30819320b3",
+    "url": "/React-Recipes/static/css/main.971c5d69.chunk.css"
+  },
+  {
+    "revision": "ddd4e623390ec3facaf3",
+    "url": "/React-Recipes/static/js/2.77b90360.chunk.js"
+  },
+  {
+    "revision": "c7993d6f2f30819320b3",
+    "url": "/React-Recipes/static/js/main.b11302ca.chunk.js"
+  },
+  {
+    "revision": "749aea48d3b97b5b00cf",
+    "url": "/React-Recipes/static/js/runtime~main.4b118369.js"
+  }
+]);
